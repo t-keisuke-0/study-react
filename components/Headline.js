5031e7e5b1a5e5cea124c014f659160c43912036
t-keisuke-0 <kasakokey0728@gmail.com>
2025-05-11 11:54:41 +0900

    ヘッドライン部分をコンポーネント化

---
+++ components/Headline.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+
+
+export function Headline(props) {
+  return (
+    <div>
+        <h1 className={styles.title}>{props.page} page</h1>
+        <Image
+            className={styles.logo}
+            src="/next.svg"
+            alt="Next.js logo"
+            width={180}
+            height={38}
+            priority
+        />
+        <ol>
+            <li>
+            Get started by editing <code>pages/{props.page}.js</code>.
+            </li>
+            <li>Save and see your changes instantly.</li>
+        </ol>
+    </div>
+  )
+}
