6035997c97f9daf5f732512c5a6d5c38a4481d1c
t-keisuke-0 <kasakokey0728@gmail.com>
2025-05-11 23:21:00 +0900

    拡張子・ディレクトリ構成・インポート方法の変更、_appを用いた処理共通化

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,12 +1,12 @@
 import classes from "./Main.module.css";
-import { Links } from "@/components/Links";
-import { Headline } from "@/components/Headline";
+import { Links } from "@/src/components/Links";
+import { Headline } from "@/src/components/Headline";
 
 export function Main(props) {
   return (
     <main className={classes.main}>
         <Headline page={props.page}>
-            <code>pages/{props.page}.js</code>
+            <code>pages/{props.page}.jsx</code>
         </Headline>
         <Links />
     </main>
