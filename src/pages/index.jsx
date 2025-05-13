b8fc688d23bc38893f01d6330d2de0cfded8e419
t-keisuke-0 <kasakokey0728@gmail.com>
2025-05-13 21:38:39 +0900

    試しにクリックイベント作成

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,6 +4,7 @@ import styles from "@/src/styles/Home.module.css";
 import { Footer } from "@/src/components/Footer";
 import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
+import { useCallback } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,13 +16,31 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+// const handleClick = (e) => {
+//   console.log(e.target.href)
+//   e.preventDefault()
+    // alert(foo)
+// }
+
+
 export default function Home() {
+  const foo = 1
+
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href)
+    e.preventDefault()
+    alert(foo)
+  }, [])
+
   return (
     <div>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <div
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
