7beeda69c0d9a69998f41d442a4a7ec0330310cb
t-keisuke-0 <kasakokey0728@gmail.com>
2025-05-15 22:09:23 +0900

    useStateで文字列と真偽値

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -19,12 +19,26 @@ const geistMono = Geist_Mono({
 
 export default function Home() {
   const [count, setCount] = useState(1)
+  const [text, setText] = useState('')
+  const [isShow, setIsShow] = useState(true)
 
   const handleClick = useCallback(() => {
     if (count < 10) {
       setCount((count) => count + 1);
     }
   }, [count])
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow)
+  }, [])
+
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert('5文字以内にしてください')
+      return;
+    }
+    setText(e.target.value.trim())
+  }, [])
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
@@ -39,10 +53,10 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      <h1>{count}</h1>
-      <button onClick={handleClick}>
-        ボタン
-      </button>
+      {isShow ? <h1>{count}</h1>: null}
+      <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示": "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
       <div
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
