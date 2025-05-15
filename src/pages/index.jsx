4dadf8dd480013f2951a181c90955f2a0d02a192
t-keisuke-0 <kasakokey0728@gmail.com>
2025-05-15 21:46:53 +0900

    useCallback, useEffectの第2引数

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,7 @@ import styles from "@/src/styles/Home.module.css";
 import { Footer } from "@/src/components/Footer";
 import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,19 +16,15 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-// const handleClick = (e) => {
-//   console.log(e.target.href)
-//   e.preventDefault()
-    // alert(foo)
-// }
 
 export default function Home() {
   const [count, setCount] = useState(1)
 
-  const handleClick = (e) => {
-    setCount(count => count + 1);
-    setCount(count => count + 1);
-  }
+  const handleClick = useCallback(() => {
+    if (count < 10) {
+      setCount((count) => count + 1);
+    }
+  }, [count])
 
   useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
