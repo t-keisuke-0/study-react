d17a5cf1aa00983e5b42b8a2d0f66d2efa442af5
t-keisuke-0 <kasakokey0728@gmail.com>
2025-05-19 22:07:16 +0900

    ページ間のStateのリフトアップ

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,15 @@
 import Head from "next/head";
 import "@/src/styles/globals.css";
+import { useCounter } from "@/src/hooks/useCounter";
+import { useInputArray } from "@/src/hooks/useInputArray";
+import { useBgLightBlue } from "@/src/hooks/useBgLightBlue";
+
 
 export default function App({ Component, pageProps }) {
+  const counter = useCounter()
+  const inputArray = useInputArray()
+  useBgLightBlue()
+
   return (
     <>
       <Head>
@@ -9,7 +17,7 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Component {...pageProps} />
+      <Component {...pageProps} {...counter} {...inputArray} />
     </>
   );
 }
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,9 +4,6 @@ import styles from "@/src/styles/Home.module.css";
 import { Footer } from "@/src/components/Footer";
 import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
-import { useCounter } from "@/src/hooks/useCounter";
-import { useInputArray } from "@/src/hooks/useInputArray";
-import { useBgLightBlue } from "@/src/hooks/useBgLightBlue";
 
 
 const geistSans = Geist({
@@ -19,10 +16,10 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export default function About() {
-  const { count, isShow, handleClick, handleDisplay } = useCounter()
-  const { text, array, handleChange, handleAdd } = useInputArray()
-  useBgLightBlue()
+export default function About({
+    count, isShow, handleClick, handleDisplay,
+    text, array, handleChange, handleAdd
+  }) {
 
   return (
     <div>
