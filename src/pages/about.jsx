b22d62a7ca5cb0c828ba8435f54148b06aad2864
t-keisuke-0 <kasakokey0728@gmail.com>
2025-05-19 21:47:30 +0900

    Custom Hooks仕様

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,6 +4,9 @@ import styles from "@/src/styles/Home.module.css";
 import { Footer } from "@/src/components/Footer";
 import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
+import { useCounter } from "@/src/hooks/useCounter";
+import { useInputArray } from "@/src/hooks/useInputArray";
+import { useBgLightBlue } from "@/src/hooks/useBgLightBlue";
 
 
 const geistSans = Geist({
@@ -16,13 +19,29 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export default function Home() {
+export default function About() {
+  const { count, isShow, handleClick, handleDisplay } = useCounter()
+  const { text, array, handleChange, handleAdd } = useInputArray()
+  useBgLightBlue()
+
   return (
     <div>
       <Head>
         <title>About Page</title>
       </Head>
       <Header />
+      {isShow ? <h1>{count}</h1>: null}
+      <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示": "表示"}</button>
+      <input type="text" value={text} onChange={handleChange} />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map(item =>{
+          return (
+            <div key={item}>{item}</div>
+          )
+        })}
+      </ul>
       <div
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,7 +4,9 @@ import styles from "@/src/styles/Home.module.css";
 import { Footer } from "@/src/components/Footer";
 import { Main } from "@/src/components/Main";
 import { Header } from "@/src/components/Header";
-import { useCallback, useEffect, useState } from "react";
+import { useCounter } from "@/src/hooks/useCounter";
+import { useInputArray } from "@/src/hooks/useInputArray";
+import { useBgLightBlue } from "@/src/hooks/useBgLightBlue";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,47 +18,10 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-
 export default function Home() {
-  const [count, setCount] = useState(1)
-  const [text, setText] = useState('')
-  const [isShow, setIsShow] = useState(true)
-  const [array, setArray] = useState([])
-
-  const handleClick = useCallback(() => {
-    if (count < 10) {
-      setCount((prevCount) => prevCount + 1);
-    }
-  }, [count])
-
-  const handleDisplay = useCallback(() => {
-    setIsShow((prevIsShow) => !prevIsShow)
-  }, [])
-
-  const handleChange = useCallback((e) => {
-    if (e.target.value.length > 5) {
-      alert('5文字以内にしてください')
-      return;
-    }
-    setText(e.target.value.trim())
-  }, [])
-
-  const handleAdd = useCallback(() => {
-    setArray((prevArray) => {
-      if (prevArray.some(item => item === text)) {
-        alert("同じ要素が既に存在します。")
-        return prevArray
-      }
-      return [...prevArray, text]
-    }) 
-  }, [text])
-
-  useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
-    return () => {
-      document.body.style.backgroundColor = "";
-    }
-  }, []);
+  const { count, isShow, handleClick, handleDisplay } = useCounter()
+  const { text, array, handleChange, handleAdd } = useInputArray()
+  useBgLightBlue()
 
   return (
     <div>
@@ -64,9 +29,11 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
+
       {isShow ? <h1>{count}</h1>: null}
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示": "表示"}</button>
+
       <input type="text" value={text} onChange={handleChange} />
       <button onClick={handleAdd}>追加</button>
       <ul>
@@ -76,6 +43,7 @@ export default function Home() {
           )
         })}
       </ul>
+  
       <div
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
