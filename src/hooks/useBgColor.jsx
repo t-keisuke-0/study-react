5842883658698e135eb095b5650829a10c5620b6
t-keisuke-0 <kasakokey0728@gmail.com>
2025-05-20 20:38:06 +0900

    useRouter

---
+++ src/hooks/useBgColor.jsx
@@ -0,0 +1,30 @@
+import { useRouter } from "next/router";
+import { useEffect, useMemo } from "react";
+
+
+export const useBgColor = () => {
+  const router = useRouter()
+
+  console.log(router)
+
+  const beColor = useMemo(() => {
+    switch (router.pathname) {
+      case '/': {
+        return 'lightblue'
+      }
+      case '/about': {
+        return 'beige'
+      }
+      default: {
+        return ''
+      }
+    }
+  }, [router.pathname])
+
+  useEffect(() => {
+    document.body.style.backgroundColor = beColor;
+    return () => {
+      document.body.style.backgroundColor = "";
+    }
+  }, [beColor]);
+}
