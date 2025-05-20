fcb2a405fa906d0d2e81cea42a19141d7f1c4f26
t-keisuke-0 <kasakokey0728@gmail.com>
2025-05-20 20:29:08 +0900

    useMemo

--- src/hooks/useCounter.jsx
+++ src/hooks/useCounter.jsx
@@ -1,8 +1,13 @@
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 
 export const useCounter = () => {
   const [count, setCount] = useState(1)
   const [isShow, setIsShow] = useState(true)
+
+  // const doubleCount = count * 2
+  const doubleCount = useMemo(() => {
+    return count * 2
+  }, [count])
 
   const handleClick = useCallback(() => {
     if (count < 10) {
@@ -14,5 +19,5 @@ export const useCounter = () => {
     setIsShow((prevIsShow) => !prevIsShow)
   }, [])
 
-  return { count, isShow, handleClick, handleDisplay }
+  return { count, doubleCount, isShow, handleClick, handleDisplay }
 }
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -17,7 +17,7 @@ const geistMono = Geist_Mono({
 });
 
 export default function About({
-    count, isShow, handleClick, handleDisplay,
+    doubleCount, isShow, handleClick, handleDisplay,
     text, array, handleChange, handleAdd
   }) {
 
@@ -29,7 +29,7 @@ export default function About({
       <Header />
       <button onClick={handleClick}>ボタン</button>
       <button onClick={handleDisplay}>{isShow ? "非表示": "表示"}</button>
-      {isShow ? <h2>{count}</h2>: null}
+      {isShow ? <h2>{doubleCount}</h2>: null}
       <hr />
       <input type="text" value={text} onChange={handleChange} />
       <button onClick={handleAdd}>追加</button>
